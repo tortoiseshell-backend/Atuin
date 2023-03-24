48375b466103e2dad5d3ef84d5d610c788a9ffb4
JakeSifo <bearcoco5@gmail.com>
2023-03-24 05:15:31 -0400

    Conected sort to reviews, refactored how sort and review slices function.

--- client/src/reducers/sortSlice.js
+++ client/src/reducers/sortSlice.js
@@ -1,4 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
+import {
+  getReviewsAsync,
+} from '@reducers/reviewSlice';
 
 export const sortSlice = createSlice({
   name: 'sort',
@@ -11,6 +14,11 @@ export const sortSlice = createSlice({
     },
   },
 });
+
+export const toggleSort = (sortedBy) => async (dispatch) => {
+  dispatch(sortSlice.actions.toggle(sortedBy));
+  dispatch(getReviewsAsync());
+};
 
 export default sortSlice.reducer;
 export const { toggle } = sortSlice.actions;
