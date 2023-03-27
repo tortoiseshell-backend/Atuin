79a493b359e52bb1ad334217f7378a11d3c085de
Jessica Tong <120988755+jessicatong43@users.noreply.github.com>
2023-03-26 21:14:51 -0700

    Indented lines 17-32.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -14,22 +14,22 @@ function App() {
     <div id="app" className={appStyles}>
       <style>
         {`
-        body::-webkit-scrollbar {
-          width: 2vh;
-          height: 2vh;
-        }
-        body::-webkit-scrollbar-thumb {
-          background-color: rgb(97 32 216);
-          border-radius: 6px;
-        }
-        body::-webkit-scrollbar-button {
-          display: none;
-        }
-        body::-webkit-scrollbar-track {
-          background-color: #e5f4ff;
-          border-radius: 6px;
-        }
-      `}
+          body::-webkit-scrollbar {
+            width: 2vh;
+            height: 2vh;
+          }
+          body::-webkit-scrollbar-thumb {
+            background-color: rgb(97 32 216);
+            border-radius: 6px;
+          }
+          body::-webkit-scrollbar-button {
+            display: none;
+          }
+          body::-webkit-scrollbar-track {
+            background-color: #e5f4ff;
+            border-radius: 6px;
+          }
+        `}
       </style>
       I am the App div
       <div className="row-span-1">
