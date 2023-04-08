d5d5e7b50ed6787c96fd4908cee35b72d1a423b3
Markus Leben <markus_leben@mac.com>
2023-04-07 23:53:04 -0700

    fixed server pw

--- QnA/postgres/db.js
+++ QnA/postgres/db.js
@@ -5,14 +5,14 @@ const pw = process.env.POSTGRESSPW
 
 
 const db = pgPromise({
-  user: 'Markus',
-  host: 'localhost',
-  database: 'qna'
-  // user: 'qnauser',
-  // host: 'ec2-54-209-182-101.compute-1.amazonaws.com',
-  // database: 'qna',
-  // port: 5432,
-  // password: pw,
+  // user: 'Markus',
+  // host: 'localhost',
+  // database: 'qna'
+  user: 'qnauser',
+  host: 'ec2-54-209-182-101.compute-1.amazonaws.com',
+  database: 'qna',
+  port: 5432,
+  password: pw,
 })
 
 module.exports = db;
