e99f2a733d51501e57bc3b7a5408a252bcf5ab77
JakeSifo <bearcoco5@gmail.com>
2023-04-13 14:31:08 -0400

    More edits

--- ProductOverviewAPI/db/db.js
+++ ProductOverviewAPI/db/db.js
@@ -3,7 +3,11 @@ const pgPromise = require('pg-promise')();
 require('dotenv').config();
 
 const db = pgPromise({
-  connectionString: process.env.PRODUCTS_DATABASE_URL,
+  user: 'ubuntu',
+  host: 'ec2-18-219-72-33.us-east-2.compute.amazonaws.com',
+  database: 'products',
+  password: 'password',
+  port: 5432
 });
 
 module.exports = db;
