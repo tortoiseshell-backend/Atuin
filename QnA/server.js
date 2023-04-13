aed4f48e9ea9a7b49d3558cd0bcd062c981ad63e
Markus Leben <markus_leben@mac.com>
2023-04-13 14:52:21 -0700

    loaderio change

--- QnA/server.js
+++ QnA/server.js
@@ -19,11 +19,11 @@ app.put('/api/qa/questions/:question_id/helpful', controllers.HelpfulQuestion)
 app.put('/api/qa/answers/:answer_id/helpful', controllers.HelpfulAnswer)
 app.put('/api/qa/questions/:question_id/report', controllers.ReportQuestion)
 app.put('/api/qa/answers/:answer_id/report', controllers.ReportAnswer)
-app.get('/loaderio-625054bd19e4b11ed26eec0a418be350', (req, res) => {
+app.get('/loaderio-8b78ade6ad516ed738be79787853fb4b', (req, res) => {
   res.set({"Content-Disposition":
-          "attachment; filename=\"loaderio-625054bd19e4b11ed26eec0a418be350.txt\""
+          "attachment; filename=\"loaderio-8b78ade6ad516ed738be79787853fb4b.txt\""
 })
-  res.send('loaderio-625054bd19e4b11ed26eec0a418be350')}) //loaderio auth path
+  res.send('loaderio-8b78ade6ad516ed738be79787853fb4b')}) //loaderio auth path
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
