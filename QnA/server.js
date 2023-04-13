0ddd3f8d3fbc2223729c7cf41010e11b1d85a813
Markus Leben <markus_leben@mac.com>
2023-04-13 10:45:32 -0700

    added loaderio auth path

--- QnA/server.js
+++ QnA/server.js
@@ -19,6 +19,11 @@ app.put('/api/qa/questions/:question_id/helpful', controllers.HelpfulQuestion)
 app.put('/api/qa/answers/:answer_id/helpful', controllers.HelpfulAnswer)
 app.put('/api/qa/questions/:question_id/report', controllers.ReportQuestion)
 app.put('/api/qa/answers/:answer_id/report', controllers.ReportAnswer)
+app.get('/loaderio-625054bd19e4b11ed26eec0a418be350', (req, res) => {
+  res.set({"Content-Disposition":
+          "attachment; filename=\"loaderio-625054bd19e4b11ed26eec0a418be350.txt\""
+})
+  res.send('loaderio-625054bd19e4b11ed26eec0a418be350')}) //loaderio auth path
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
