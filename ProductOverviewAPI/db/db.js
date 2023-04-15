2655d106d8f4089328200a311eb653b04153a328
JakeSifo <bearcoco5@gmail.com>
2023-04-15 02:08:59 -0400

    Test 5

--- ProductOverviewAPI/db/db.js
+++ ProductOverviewAPI/db/db.js
@@ -7,7 +7,8 @@ const db = pgPromise({
   host: process.env.HOST,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  idleTimeoutMillis: 5000,
 });
 
 module.exports = db;
--- ProductOverviewAPI/server/controllers.js
+++ ProductOverviewAPI/server/controllers.js
@@ -1,26 +1,29 @@
 const db = require('../db/db.js')
+const pgp = require('pg-promise')();
 
 exports.products = async (req, res) => {
+  const getProductsQuery = pgp.prepare(`
+    SELECT json_agg(
+      json_build_object(
+        'id', p.id,
+        'name', p.name,
+        'slogan', p.slogan,
+        'description', p.description,
+        'category', p.category,
+        'default_price', p.default_price
+      )
+    ) AS products
+    FROM products.products p
+    WHERE p.id BETWEEN $1 AND $2
+  `);
+
   const page = parseInt(req.query.page) || 1;
   const count = parseInt(req.query.count) || 5;
   const offset = (page - 1) * count;
 
   try {
-    // Use `json_agg` to aggregate product information into a single JSON object
-    const results = await db.one(`
-      SELECT json_agg(
-        json_build_object(
-          'id', p.id,
-          'name', p.name,
-          'slogan', p.slogan,
-          'description', p.description,
-          'category', p.category,
-          'default_price', p.default_price
-        )
-      ) AS products
-      FROM products.products p
-      WHERE p.id BETWEEN $1 AND $2
-    `, [offset + 1, offset + count]);
+    // Use the prepared statement to avoid parsing the SQL every time the query is executed
+    const results = await db.one(getProductsQuery, [offset + 1, offset + count]);
 
     // Send the products JSON object
     res.send(results.products);
@@ -68,59 +71,58 @@ exports.product = async (req, res) => {
   }
 };
 
-
-exports.styles = (req, res) => {
-  db.any(`
-      SELECT json_build_object(
-        'product_id', $1,
-        'results', json_agg(
-          json_build_object(
-            'style_id', s.id,
-            'name', s.name,
-            'original_price', s.original_price::text,
-            'sale_price', s.sale_price::text,
-            'default?', s.default_style = '1',
-            'photos', (
-              SELECT COALESCE(json_agg(
-                json_build_object(
-                  'thumbnail_url', p.thumbnail_url,
-                  'url', p.url
+exports.styles = async (req, res) => {
+  try {
+    const result = await db.tx(async (t) => {
+      const query = `
+        SELECT json_build_object(
+          'product_id', $1,
+          'results', json_agg(
+            json_build_object(
+              'style_id', s.id,
+              'name', s.name,
+              'original_price', s.original_price::text,
+              'sale_price', s.sale_price::text,
+              'default?', s.default_style = '1',
+              'photos', (
+                SELECT COALESCE(json_agg(
+                  json_build_object(
+                    'thumbnail_url', p.thumbnail_url,
+                    'url', p.url
+                  )
+                  ORDER BY p.id ASC
+                ), '[]')
+                FROM products.photos p
+                WHERE p.style_id = s.id
+              ),
+              'skus', (
+                SELECT json_object_agg(
+                  sk.id::text,
+                  json_build_object(
+                    'size', sk.size,
+                    'quantity', sk.quantity
+                  )
                 )
-                ORDER BY p.id ASC
-              ), '[]')
-              FROM products.photos p
-              WHERE p.style_id = s.id
-            ),
-            'skus', (
-              SELECT json_object_agg(
-                sk.id::text,
-                json_build_object(
-                  'size', sk.size,
-                  'quantity', sk.quantity
-                )
+                FROM products.skus sk
+                WHERE sk.style_id = s.id
               )
-              FROM products.skus sk
-              WHERE sk.style_id = s.id
             )
+            ORDER BY s.id ASC
           )
-          ORDER BY s.id ASC
         )
-      )
-      FROM products.styles s
-      WHERE s.product_id = $1
-      GROUP BY s.product_id
-  `, [req.params.product_id])
-    .then((results) => {
-
-      res.send(results);
-
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send(error + '\nError getting styles');
+        FROM products.styles s
+        WHERE s.product_id = $1
+        GROUP BY s.product_id
+      `;
+      return t.any(query, [req.params.product_id]);
     });
-};
 
+    res.send(result);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send(e + '\nError getting styles');
+  }
+};
 
 exports.related = (req, res) => {
   db.any(`
